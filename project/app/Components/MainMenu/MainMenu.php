f80a92b16fdb2c84ddd0c0f7efbf29e542240d39
frunolu <fruno.lu@gmail.com>
2023-02-07 17:01:11 +0100

    Create component MainMenu

---
+++ project/app/Components/MainMenu/MainMenu.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Components\MainMenu;
+
+use Nette\Application\UI\Control;
+
+class MainMenu extends Control
+{
+    /**
+     * @var array
+     */
+    private array $links = [];
+
+    /**
+     * @param array<array{url:string, key:string}> $links
+     */
+    public function __construct(array $links)
+    {
+        foreach ($links as $link) {
+            $this->addLink($link['url'], $link['name']);
+        }
+    }
+
+    public function render(): void
+    {
+        $this->getTemplate()->setFile(__DIR__.'/templates/MainMenu.latte');
+        $this->getTemplate()->links = $this->links;
+        $this->getTemplate()->render();
+    }
+
+    public function addLink(string $url, string $name): void
+    {
+        $this->links[] = [
+            'url' => $url,
+            'name' => $name,
+
+        ];
+    }
+
+}
--- project/app/Presenters/BasePresenter.php
+++ project/app/Presenters/BasePresenter.php
@@ -4,9 +4,25 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use App\Components\MainMenu\MainMenu;
 use Nette\Application\UI\Presenter;
 
 class BasePresenter extends Presenter
 {
+    public function createComponentMainMenu(): MainMenu
+    {
+        $links = [
+            [
+                'url' => 'pekarna-a-cukrarna',
+                'name' => 'Pekárna a cukrárna',
+            ],
+            [
+                'url' => 'maso-a-ryby',
+                'name' => 'Maso a ryby',
+            ],
+        ];
+
+        return new MainMenu($links);
+    }
 
 }
