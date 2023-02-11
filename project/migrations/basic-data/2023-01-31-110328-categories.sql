c691b5c12c99ad9f57457f7f90e2906eeb56019c
frunolu <fruno.lu@gmail.com>
2023-02-11 14:42:01 +0100

    Create migration table category via nextras migration

---
+++ project/migrations/basic-data/2023-01-31-110328-categories.sql
@@ -0,0 +1,16 @@
+INSERT INTO `category` (`id`, `parent_id`, `name`, `url`, `created`, `updated`, `deleted`)
+VALUES (1, NULL, 'Pekárna a cukrárna', 'pekarna-cukrarna', now(), NULL, '0'),
+       (2, NULL, 'Ovoce a zelenina', 'ovoce-a-zelenina', now(), NULL, '0'),
+       (3, NULL, 'Maso a ryby', 'maso-a-ryby', now(), NULL, '0'),
+       (4, NULL, 'Mléčné a chlazené', 'mlecne-a-chlazene', now(), NULL, '0'),
+       (5, NULL, 'Nápoje', 'napoje', now(), NULL, '0'),
+       (6, 1, 'Chléb', 'chleb', now(), NULL, '0'),
+       (7, 1, 'Sladké pečivo', 'sladke-pecivo', now(), NULL, '0'),
+       (8, 2, 'Zelenina', 'zelenina', now(), NULL, '0'),
+       (9, 2, 'Ovoce', 'ovoce', now(), NULL, '0'),
+       (10, 3, 'Hovězí maso', 'hovezi-maso', now(), NULL, '0'),
+       (11, 3, 'Drůbeží maso', 'drubezi-maso', now(), NULL, '0'),
+       (12, 4, 'Tvarohy', 'tvarohy', now(), NULL, '0'),
+       (13, 4, 'Jogurty a mléčné dezerty', 'jogurty-a-mlecne-dezerty', now(), NULL, '0'),
+       (14, 5, 'Vína', 'vina', now(), NULL, '0'),
+       (15, 5, 'Čaje', 'caje', now(), NULL, '0');
