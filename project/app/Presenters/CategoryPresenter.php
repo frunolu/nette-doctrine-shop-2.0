ad7ee3cefe2fae98735038cd4e2ff6f2a3038db8
frunolu <fruno.lu@gmail.com>
2023-02-11 15:52:25 +0100

    Create CategoryPresenter with template

--- project/app/Presenters/CategoryPresenter.php
+++ project/app/Presenters/CategoryPresenter.php
@@ -4,7 +4,43 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use App\Model\Category\Category;
+use App\Model\Category\CategoryNotFoundException;
+use App\Model\Category\CategoryRepository;
+use Nette\Application\BadRequestException;
+
 final class CategoryPresenter extends BasePresenter
 {
+    private CategoryRepository $categoryRepository;
 
+    private ?Category $category = null;
+
+    public function __construct(CategoryRepository $categoryRepository)
+    {
+        parent::__construct();
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    /**
+     * @param string $slug
+     * @return void
+     * @throws BadRequestException
+     */
+    public function actionDefault(string $slug): void
+    {
+        try {
+            $this->category = $this->categoryRepository->getByUrl($slug);
+        } catch (CategoryNotFoundException $e) {
+            $this->error();
+        }
+    }
+
+    public function renderDefault(): void
+    {
+        $this->template->setParameters([
+            'categoryId' => $this->category->getId(),
+            'categoryName' => $this->category->getName(),
+        ]);
+    }
 }
+
