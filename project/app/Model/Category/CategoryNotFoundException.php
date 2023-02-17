2546b8ff9e72db8c0e0a1f73bb1eb618c812927a
frunolu <fruno.lu@gmail.com>
2023-02-17 14:34:27 +0100

    Create CategoryRepository and CategoryNotFoundException

---
+++ project/app/Model/Category/CategoryNotFoundException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Model\Repository\Category;
+
+use Exception;
+
+final class CategoryNotFoundException extends Exception
+{
+    public static function byPrimaryKey(int $productId): self
+    {
+        return new self(sprintf('Category with id: %d not found', $productId));
+    }
+
+    public static function byUrl(string $url): self
+    {
+        return new self(sprintf('Category with url: %s not found', $url));
+    }
+}
