bc389eb5d212d8358d88f9126710ddf9e209eb5e
frunolu <fruno.lu@gmail.com>
2023-02-17 14:36:43 +0100

    Edit BasePresenter

--- project/app/Presenters/BasePresenter.php
+++ project/app/Presenters/BasePresenter.php
@@ -5,24 +5,50 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use App\Components\MainMenu\MainMenu;
+use App\Model\Category\CategoryRepository;
+use App\Model\Category\Category;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Tools\Setup;
 use Nette\Application\UI\Presenter;
+use Nette\Database\Context;
+use Tracy\Debugger;
+use App\Model\BaseRepository;
 
 class BasePresenter extends Presenter
 {
+    /** @var CategoryRepository @inject */
+    public CategoryRepository $categoryRepository;
+    /** @var BaseRepository @inject */
+    public BaseRepository $baseRepository;
+
+    public function __construct(BaseRepository $baseRepository, CategoryRepository $categoryRepository)
+    {
+        parent::__construct();
+        $this->baseRepository = $baseRepository;
+        $this->categoryRepository = $categoryRepository;
+    }
     public function createComponentMainMenu(): MainMenu
     {
-        $links = [
+        $em = $this->baseRepository->getEntityManager();
+        $category = $em->getRepository(Category::class);
+
+        $links = [];
+        $categories = $category->findBy(    // $category->findAll();
             [
-                'url' => 'pekarna-a-cukrarna',
-                'name' => 'Pekárna a cukrárna',
+                'parent' => null,
             ],
-            [
-                'url' => 'maso-a-ryby',
-                'name' => 'Maso a ryby',
-            ],
-        ];
+        );
+        foreach ($categories as $category) {
+            $links[] = [
+                'url' => $category->getUrl(),
+                'name' => $category->getName(),
+            ];
+        }
+
+        Debugger::barDump($links);
 
         return new MainMenu($links);
     }
-
 }
+
