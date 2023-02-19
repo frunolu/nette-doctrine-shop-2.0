be1a1e601dc5a5797f7ad44c746b086c9c63db93
frunolu <fruno.lu@gmail.com>
2023-02-19 11:18:41 +0100

    create OrderPresenter

---
+++ project/app/FrontModule/Presenters/OrderPresenter.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\FrontuModule\Presenters;
+
+use App\Model\Order\Order;
+use App\Events\OrderCreatedEvent;
+use App\Model\Order\OrderRepository;
+use App\Presenters\BasePresenter;
+use Nette\Application\UI\Form;
+
+class OrderPresenter extends BasePresenter
+{
+    protected OrderRepository $orderRepository;
+
+    public function __construct(OrderRepository $orderRepository)
+    {
+        parent::__construct();
+        $this->orderRepository = $orderRepository;
+    }
+
+
+    public function createComponentOrderForm(): Form
+    {
+        $form = new Form();
+
+        // Add form fields here...
+
+        $form->addSubmit('submit', 'Place order');
+
+        $form->onSuccess[] = function (Form $form, array $values) {
+            $order = new Order();
+            $order->setUser($this->getUser()->getIdentity());
+            $order->setAddress($values['address']);
+            $order->setCity($values['city']);
+            $order->setCountry($values['country']);
+            $order->setZip($values['zip']);
+            $order->setPhone($values['phone']);
+            $order->setEmail($values['email']);
+            $order->setComment($values['comment']);
+            $order->setCreatedAt(new \DateTime());
+            $order->setUpdatedAt(new \DateTime());
+
+            $this->orderRepository->save($order);
+
+            $this->getEventDispatcher()->dispatch(new OrderCreatedEvent($order));
+
+            $this->flashMessage('Your order has been placed successfully.', 'success');
+            $this->redirect('Homepage:default');
+        };
+        return $form;
+    }}
