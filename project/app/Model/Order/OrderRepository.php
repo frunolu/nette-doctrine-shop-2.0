1aaf63f5f913a3b53db0e9062a0fbd684146f882
frunolu <fruno.lu@gmail.com>
2023-02-19 11:47:03 +0100

    edit OrderRepository

--- project/app/Model/Order/OrderRepository.php
+++ project/app/Model/Order/OrderRepository.php
@@ -6,6 +6,8 @@ use Doctrine\ORM\EntityRepository;
 use App\Model\Order\Order;
 use App\Events\OrderCreatedEvent;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
 
 class OrderRepository extends EntityRepository
 {
@@ -25,5 +27,25 @@ class OrderRepository extends EntityRepository
 
         $event = new OrderCreatedEvent($order);
         $this->dispatcher->dispatch($event);
+    }
+
+    public function findAllOrdered()
+    {
+        return $this->createQueryBuilder('o')
+            ->orderBy('o.date', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * @throws NonUniqueResultException
+     * @throws NoResultException
+     */
+    public function getTotalRevenue()
+    {
+        return $this->createQueryBuilder('o')
+            ->select('SUM(o.total) as total')
+            ->getQuery()
+            ->getSingleScalarResult();
     }
 }
