ea6dbda00c5f55d40185e59381eeaa0272ede1d6
frunolu <fruno.lu@gmail.com>
2023-02-19 10:16:09 +0100

    Create entity Order

---
+++ project/app/Model/Order/Order.php
@@ -0,0 +1,93 @@
+<?php
+
+
+namespace App\Model\Order;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="orders")
+ */
+class Order
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $customer;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $shippingAddress;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $orderDate;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     */
+    private $totalPrice;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCustomer(): ?string
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer(string $customer): self
+    {
+        $this->customer = $customer;
+
+        return $this;
+    }
+
+    public function getShippingAddress(): ?string
+    {
+        return $this->shippingAddress;
+    }
+
+    public function setShippingAddress(string $shippingAddress): self
+    {
+        $this->shippingAddress = $shippingAddress;
+
+        return $this;
+    }
+
+    public function getOrderDate(): ?\DateTimeInterface
+    {
+        return $this->orderDate;
+    }
+
+    public function setOrderDate(\DateTimeInterface $orderDate): self
+    {
+        $this->orderDate = $orderDate;
+
+        return $this;
+    }
+
+    public function getTotalPrice(): ?float
+    {
+        return $this->totalPrice;
+    }
+
+    public function setTotalPrice(float $totalPrice): self
+    {
+        $this->totalPrice = $totalPrice;
+
+        return $this;
+    }
+}
