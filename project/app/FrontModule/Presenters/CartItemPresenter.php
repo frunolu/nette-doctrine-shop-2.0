d88a53db573340c948a132d18fcbc3f5e98bbf7e
frunolu <fruno.lu@gmail.com>
2023-02-19 13:15:30 +0100

    Create CartItemPresenter

--- project/app/FrontModule/Presenters/CartItemPresenter.php
+++ project/app/FrontModule/Presenters/CartItemPresenter.php
@@ -6,12 +6,13 @@ namespace App\FrontModule\Presenters;
 use App\Model\Cart\CartItemRepository;
 use Nette\Application\UI\Presenter;
 
-class CartPresenter extends Presenter
+class CartItemPresenter extends Presenter
 {
     private CartItemRepository $cartItemRepository;
 
     public function __construct(CartItemRepository $cartItemRepository)
     {
+        parent::__construct();
         $this->cartItemRepository = $cartItemRepository;
     }
 
